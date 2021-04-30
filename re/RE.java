fa3723f3f63be44ed1c0dfa2a177ce5382a34126
JeremyBouchard220 <60022660+JeremyBouchard220@users.noreply.github.com>
2021-04-30 16:00:12 -0600

    RE update #1
    "Completed" all methods except star() method.

---
+++ re/RE.java
@@ -0,0 +1,183 @@
+package re;
+
+import fa.nfa.NFA;
+import fa.State;
+import fa.nfa.NFAState;
+
+import java.util.*;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class RE implements REInterface 
+{
+    int incrementState = 0;
+    String regEx;
+
+    public NFA getNFA() 
+    {
+        return regEx();
+    }
+
+    private NFA regEx()
+    {
+        NFA term = term();
+
+        if (regEx.length() > 0 && peek() == '|')
+        {
+            eat('|');
+            NFA regEx = regEx();
+            return  union(term, regEx);
+        }
+
+        else
+        {
+            return term;
+        }
+    }
+    
+    public RE(String regEx)
+    {
+        this.regEx = regEx;
+    }
+
+    private NFA term()
+    {
+        NFA beginningFactor = new NFA();
+
+        while (regEx.length() > 0 && peek() != ')' && peek() != '|')
+        {
+            NFA newFactor = factor();
+
+            if (!startFactor.getStates().isEmpty())
+            {
+                beginningFactor = concat(beginningFactor, newFactor)
+            }
+
+            else
+            {
+                beginningFactor = newFactor;
+            }
+        }
+    }
+
+    //If when combining NFA's a problem occurs, change order of addNFAStates, etc.
+    private NFA union(NFA nfa1, NFA nfa2)
+    {
+        NFA result = new NFA();
+
+        String beginningState = String.valueOf(incrementState++);
+        result.addStartState(beginningState);
+
+        result.addNFAStates(nfa1.getStates());
+        result.addTransition(beginningState, 'e', nfa1.getStartState().getName());
+        result.addAbc(nfa1.getABC());
+
+        result.addNFAStates(nfa2.getStates());
+        result.addTransition(beginningState, 'e', nfa2.getStartState().getName());
+        result.addAbc(nfa2.getABC());
+
+        return result;
+    }
+
+    private NFA factor()
+    {
+        NFA base = base();
+
+        while (regEx.length() > 0 && peek() == '*')
+        {
+            eat('*');
+            base = star(base);
+        }
+
+        return base;
+    }
+
+    private NFA star(NFA base)
+    {
+
+    }
+
+    private NFA concat(NFA nfa1, NFA nfa2)
+    {
+        Set<State> nfa1FinalStates = nfa1.getFinalStates();
+        nfa1.addNFAStates(nfa2.getStates());
+        nfa1.addAbc(nfa2.getABC());
+
+        Iterator<State> itr = nfa1FinalStates.iterator();
+
+        while(itr.hasNext())
+        {
+            State state = itr.next();
+            ((NFAState) state).setNonFinal();
+            nfa1.addTransition(set.getName(), 'e', nfa2.getStartState().getName());
+        }
+
+        return nfa1;
+    }
+
+    private NFA base()
+    {
+        switch (peek())
+        {
+            case '(':
+                eat('(');
+                NFA reg = regEx();
+                eat(')');
+                return reg;
+            default:
+                return symbol(next());
+        }
+    }
+
+    //incrementState may need to be in parenthesis of "new" statements
+    private NFA symbol(char symbol)
+    {
+        NFA newNfa = new NFA();
+
+        NFAState startState = new NFAState(String.valueOf(incrementState));
+        incrementState++;
+
+        NFAState endState = new NFAState(String.valueOf(incrementState));
+        incrementState++;
+
+        newNfa.addStartState(startState.getName());
+        newNfa.addFinalState(endState.getName());
+        newNfa.addTransition(starState.getName(), symbol, endState.getName());
+
+        Set<Character> alphabet = new LinkedHashSet<Character>();
+        alphabet.add(symbol);
+        newNfa.addAbc(alphabet);
+
+        return newNfa;
+    }
+
+    private void eat(char c)
+    {
+        if (peek()==clone()
+        {
+            this.regEx = this.regEx.substring(1);
+        }
+        
+        else
+        {
+            throw new RuntimeException("Received: " + peek() + "\n" + "Expected: " + c);
+        }
+    }
+
+    private char peek()
+    {
+        return regEx.charAt(0);
+    }
+
+    private char next()
+    {
+        char c = peek();
+        eat(c);
+        return c;
+    }
+
+    // private boolean more()
+    // {
+    //     return regEx.length() > 0;
+    // }
+}
